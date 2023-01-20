57de72810cbcb38fa9fe65a434c55471f75544e7
Aventom <ravaneloe@gmail.com>
2023-01-20 22:45:07 +0700

    hero

---
+++ components/Hero.tsx
@@ -0,0 +1,82 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+import hero from "../public/ryan.jpg";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+
+type Props = {};
+
+export default function Hero({}: Props) {
+	return (
+		<div className="max-w-7xl mx-auto mt-[100px] md:mt-[200px] max-h-7xl align-middle flex items-center justify-center">
+			<div className="grid md:grid-cols-2 place-items-center gap-20">
+				<motion.div
+					initial={{
+						y: -500,
+						opacity: 0.5,
+					}}
+					animate={{
+						y: 0,
+						opacity: 1,
+					}}
+					transition={{
+						duration: 1,
+					}}
+					className="w-[250px] h-[250px] mx-auto md:mx-0 sm:w-[350px] sm:h-[350px] rounded-full overflow-hidden shadow-2xl border-primary-300 bg-primary-300 border-[20px] border-solid"
+				>
+					<Image src={hero} alt="Profile Picture" />
+				</motion.div>
+				<motion.div
+					className="sm:text-center md:text-start"
+					initial={{
+						y: 500,
+						opacity: 0.5,
+					}}
+					animate={{
+						y: 0,
+						opacity: 1,
+					}}
+					transition={{
+						duration: 1,
+					}}
+				>
+					<p className="font-poppins text-2xl text-gray-500 my-5">
+						Web Developer - Front End Engineer
+					</p>
+					<p className="font-poppins text-6xl my-5 text-primary-600">
+						Ethaniel Ravanello
+					</p>
+					<p className="font-poppins my-5 text-md text-primary-600 leading-10 ">
+						I'm a front-end engineer who just graduated from Alterra academy
+						Bootcamp. I did several projects while in the bootcamp using
+						Javascript as my main programming language and Framework/Library
+						such as ReactJS, NextJS, Tailwinds, Bootstrap and ChakraUI. And I'm
+						currently learning other frameworks and languages such as Typescript
+						and Astro.
+					</p>
+					<div className="gap-8">
+						<Link
+							className="text-primary-500 uppercase font-poppins text-xl mr-5"
+							href="#about"
+						>
+							About
+						</Link>
+						<Link
+							className="text-primary-500 uppercase font-poppins text-xl mr-5"
+							href="#skills"
+						>
+							Skills
+						</Link>
+						<Link
+							className="text-primary-500 uppercase font-poppins text-xl"
+							href="#project"
+						>
+							Project
+						</Link>
+					</div>
+				</motion.div>
+			</div>
+		</div>
+	);
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{js,ts,jsx,tsx}"],
+	content: [
+		"./src/**/*.{js,ts,jsx,tsx}",
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
 	theme: {
-		extend: {},
+		fontFamily: {
+			poppins: ["Poppins", "sans-serif"],
+		},
+		extend: {
+			colors: {
+				primary: {
+					100: "#121212",
+					200: "#181818",
+					300: "#404040",
+					400: "#282828",
+					500: "#FFFFFF",
+					600: "#B3B3B3",
+				},
+			},
+		},
 	},
 	plugins: [],
 };
