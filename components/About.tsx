cba19da6a286671b5608d048fcc62bc4681d2454
Aventom <ravaneloe@gmail.com>
2023-01-22 16:13:51 +0700

    animation

--- components/About.tsx
+++ components/About.tsx
@@ -2,16 +2,33 @@
 import React from "react";
 import Image from "next/image";
 import pict from "../public/pict.jpg";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 export default function About({}: Props) {
 	return (
 		<div className="max-w-7xl  mx-auto">
-			<p className="text-white text-center mb-10 text-4xl font-poppins">
+			<motion.p
+				initial={{
+					opacity: 0,
+				}}
+				animate={{
+					opacity: 1,
+				}}
+				className="text-white text-center mb-10 text-4xl font-poppins"
+			>
 				A B O U T
-			</p>
-			<div className="grid md:grid-cols-2 place-items-center gap-20">
+			</motion.p>
+			<motion.div
+				initial={{
+					opacity: 0,
+				}}
+				animate={{
+					opacity: 1,
+				}}
+				className="grid md:grid-cols-2 place-items-center gap-20"
+			>
 				<p className="text-primary-600 text-center md:text-start font-poppins leading-10 mt-5">
 					Hello my name is Ethaniel Ravanello, I'm a front-end engineer who just
 					graduated from Alterra academy Bootcamp. I did several projects while
@@ -21,7 +38,7 @@ export default function About({}: Props) {
 					such as Typescript and Astro.
 				</p>
 				<Image src={pict} alt="Picture" className="hidden md:inline-flex" />
-			</div>
+			</motion.div>
 		</div>
 	);
 }
