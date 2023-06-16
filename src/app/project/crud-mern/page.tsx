ce0d41a4306c5f1b3957ae75a6e86bb6357fa8b1
Ethaniel-Ravanello <ravaneloe@gmail.com>
2023-06-16 22:03:32 +0700

    feat: GP Done

--- src/app/project/crud-mern/page.tsx
+++ src/app/project/crud-mern/page.tsx
@@ -2,13 +2,11 @@ import React from "react";
 import Image from "next/image";
 import type { Metadata } from "next";
 
-import Aka1 from "@images/Project/Akarashi/Aka1.jpeg";
-import Aka2 from "@images/Project/Akarashi/Aka2.jpeg";
-import Aka3 from "@images/Project/Akarashi/Aka3.jpeg";
-import Aka4 from "@images/Project/Akarashi/Aka4.jpeg";
+import Crud1 from "@images/Project/Crud/Crud1.jpeg";
+import Crud2 from "@images/Project/Crud/Crud2.jpeg";
 
 import { FaReact } from "react-icons/fa";
-import { SiTailwindcss, SiTypescript, SiRedux } from "react-icons/si";
+import { SiTailwindcss, SiTypescript, SiNextdotjs } from "react-icons/si";
 
 export const metadata: Metadata = {
   title: "Ethaniel | Project | Akarashi",
@@ -22,37 +20,23 @@ const page = () => {
   return (
     <div className="w-full h-full  text-text ">
       <div className="pb-5 px-5 md:px-16 lg:px-[10em] xl:px-[19em]">
-        <h1 className="text-4xl lg:text-6xl font-semibold mb-10">Akarashi</h1>
+        <h1 className="text-4xl lg:text-6xl font-semibold mb-10">CRUD MERN</h1>
         <p className="max-w-[500px] leading-8">
-          A platform that allows gamers to explore a vast library of titles,
-          search for specific games based on their preferences or genres.
+          This is a project i made as a MERN Fullstack developer learning how to
+          do a basic CRUD.
         </p>
       </div>
-      <div className="bg-accent3 px-5 md:px-16 lg:px-[10em] xl:px-[19em] flex flex-wrap gap-x-20">
+      <div className="bg-accent3 px-5 md:px-16 lg:px-[10em] xl:px-[19em] py-20">
         <div className="w-full h-full lg:w-[40%] lg:h-[40%] mx-auto my-5 box-shadow shadow-lg shadow-white/30 rounded-lg">
           <Image
-            src={Aka1}
+            src={Crud1}
             alt="Foto Project"
-            className="w-full h-full object-contain rounded-lg"
+            className="w-full h-full object-contain rounded-lg mb-10"
           />
         </div>
         <div className="w-full h-full lg:w-[40%] lg:h-[40%] mx-auto mt-3 my-5 box-shadow shadow-lg shadow-white/30 rounded-lg">
           <Image
-            src={Aka2}
-            alt="Foto Project"
-            className="w-full h-full object-contain rounded-lg"
-          />
-        </div>
-        <div className="w-full h-full lg:w-[40%] lg:h-[40%] mx-auto mt-3 my-5 box-shadow shadow-lg shadow-white/30 rounded-lg">
-          <Image
-            src={Aka3}
-            alt="Foto Project"
-            className="w-full h-full object-contain rounded-lg"
-          />
-        </div>
-        <div className="w-full h-full lg:w-[40%] lg:h-[40%] mx-auto mt-3 my-5 box-shadow shadow-lg shadow-white/30 rounded-lg">
-          <Image
-            src={Aka4}
+            src={Crud2}
             alt="Foto Project"
             className="w-full h-full object-contain rounded-lg"
           />
@@ -62,7 +46,7 @@ const page = () => {
         <p className="text-4xl lg:text-6xl font-semibold mb-10">Built With</p>
         <div className="flex justify-center w-fit mx-auto gap-x-14 ">
           <div className="w-full h-full">
-            <FaReact className="w-10 h-10 md:w-16 md:h-16 lg:w-20 lg:h-20 xl:w-28 xl:h-28" />
+            <SiNextdotjs className="w-10 h-10 md:w-16 md:h-16 lg:w-20 lg:h-20 xl:w-28 xl:h-28" />
           </div>
 
           <div className="w-full h-full">
@@ -71,10 +55,6 @@ const page = () => {
 
           <div className="w-full h-full">
             <SiTypescript className="w-10 h-10 md:w-16 md:h-16 lg:w-20 lg:h-20 xl:w-28 xl:h-28" />
-          </div>
-
-          <div className="w-full h-full">
-            <SiRedux className="w-10 h-10 md:w-16 md:h-16 lg:w-20 lg:h-20 xl:w-28 xl:h-28" />
           </div>
         </div>
       </div>
